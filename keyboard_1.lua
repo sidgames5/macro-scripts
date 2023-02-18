9a099e5a8c0c49204ec47f28af2287277a18a56a
BasicCorruption <saytleader@outlook.com>
2023-02-18 10:35:48 -0500

    Update keyboard_1.lua

--- keyboard_1.lua
+++ keyboard_1.lua
@@ -1,10 +1,17 @@
 clear()
 local keyboardIdentifier = '0000AAA'
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 if keyboardIdentifier == '0000AAA' then
 	lmc_assign_keyboard('MACROS');
 else lmc_device_set_name('MACROS', keyboardIdentifier);
 end
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 dev = lmc_get_devices()
 for key,value in pairs(dev) do
   print(key..':')
@@ -16,11 +23,20 @@ print('Then replace the first 0000AAA value in the code with it. This will preve
 lmc.minimizeToTray = true
 
 sendToAHK = function (key)
+<<<<<<< Updated upstream
       local file = io.open("C:\\Users\\saytl\\Documents\\MACROOOOOOOOOOOS\\keypressed.txt", "w")
       file:write(key)
       file:flush()
       file:close()
       lmc_send_keys('{F24}')
+=======
+      --print('It was assigned string:    ' .. key)
+      local file = io.open("C:\\Users\\saytl\\Documents\\MACROOOOOOOOOOOS\\keypressed.txt", "w") -- this writes the key that was just pressed to a file that AHK will read. I'm sure there are better ways to do this but it works so I'm probably not going to change anything. Feel free to edit the code if you know a better way (and maybe make a pull request)
+      file:write(key)
+      file:flush()
+      file:close()
+      lmc_send_keys('{F24}') -- this part presses the F24 key to communicate with AHK. Again this is definitely not the best way to do it but it has been working fine for me
+>>>>>>> Stashed changes
 end
 
 local config = {
@@ -79,6 +95,10 @@ local config = {
 	[109] = "numMinus",
     [110] = "numDelete",
 	[111] = "numDiv",
+<<<<<<< Updated upstream
+=======
+    [144] = "numLock", --probably it is best to avoid this key
+>>>>>>> Stashed changes
       
     [192] = "`",
     [9]   = "tab",
@@ -124,11 +144,19 @@ local config = {
     [string.byte('8')] = "8",
     [string.byte('9')] = "9",
 
+<<<<<<< Updated upstream
 }
 
 
 lmc_set_handler('MACROS', function(button, direction)
         if (direction == 0) then return end -- ignore key upstrokes. 
+=======
+	--[255] = "printscreen" -- this does not work
+}
+
+lmc_set_handler('MACROS', function(button, direction)
+        if (direction == 0) then return end
+>>>>>>> Stashed changes
 	if type(config[button]) == "string" then
                 print(' ')
                 print('Your key ID number is:   ' .. button)
